9a38e9551685ef60806035265c8240ad2e5c4434
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-24 16:50:00 +0900

    SampleComponent実装

--- src/main/java/com/example/demo/component/SampleComponent.java
+++ src/main/java/com/example/demo/component/SampleComponent.java
@@ -1,5 +1,18 @@
 package com.example.demo.component;
 
-public class SampleComponent {
+import java.util.Random;
 
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleComponent {
+	
+	/** 0~99までの値を生成 */
+	public int random() {
+		
+		Random random = new Random();
+		int randomInt = random.nextInt(100);
+		
+		return randomInt;
+	}
 }
