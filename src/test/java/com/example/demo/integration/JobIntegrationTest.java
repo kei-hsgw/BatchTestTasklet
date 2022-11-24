8dfd3a1d546de5557e12211e14dc9ea66513e98c
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-24 20:38:34 +0900

    Jobの結合テスト

---
+++ src/test/java/com/example/demo/integration/JobIntegrationTest.java
@@ -0,0 +1,82 @@
+package com.example.demo.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.demo.BatchTestTaskletApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBatchTest
+@ContextConfiguration(classes = {BatchTestTaskletApplication.class})
+@Slf4j
+@DisplayName("JobのIntegrationTest")
+class JobIntegrationTest {
+	
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+		log.info("=== Job IntegrationTest Start ===");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		
+		log.info("=== Job IntegrationTest End ===");
+	}
+
+	@Test
+	@DisplayName("Job実行:StepとJobのステータスが完了であること")
+	void executeJob() throws Exception {
+		
+		// 準備
+		// Job起動パラメーター
+		JobParameters jobParams = new JobParametersBuilder()
+				.addString("param", "JobTest")
+				.toJobParameters();
+		
+		// テスト
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParams);
+		
+		// 検証
+		// Stepのステータス
+		jobExecution.getStepExecutions().forEach(StepExecution -> assertThat(ExitStatus.COMPLETED).isEqualTo(StepExecution.getExitStatus()));
+		
+		// Jobのステータス
+		assertThat(ExitStatus.COMPLETED).isEqualTo(jobExecution.getExitStatus());
+	}
+	
+	@Test
+	@DisplayName("Step1実行:Stepのステータスが完了であること")
+	void executeStep() throws Exception {
+		
+		// 準備
+		// Job起動パラメーター
+		JobParameters jobParams = new JobParametersBuilder()
+				.addString("param", "StepTest")
+				.toJobParameters();
+		
+		// テスト
+		JobExecution jobExecution = jobLauncherTestUtils.launchStep("Step1", jobParams);
+		
+		// 検証
+		// Stepのステータス
+		jobExecution.getStepExecutions().forEach(stepExecution -> assertThat(ExitStatus.COMPLETED).isEqualTo(stepExecution.getExitStatus()));
+	}
+
+}
