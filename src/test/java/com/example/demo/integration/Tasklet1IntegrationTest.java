c55e95df31a216d577ad20cefea5f30096647663
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-24 19:55:23 +0900

    Step結合テスト準備

---
+++ src/test/java/com/example/demo/integration/Tasklet1IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class Tasklet1IntegrationTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
