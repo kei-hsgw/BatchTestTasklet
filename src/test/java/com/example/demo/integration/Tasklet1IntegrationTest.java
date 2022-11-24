659b280df364285d800a7ff147bcf8182dfecaf6
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-24 20:19:52 +0900

    Stepの結合テスト実装

--- src/test/java/com/example/demo/integration/Tasklet1IntegrationTest.java
+++ src/test/java/com/example/demo/integration/Tasklet1IntegrationTest.java
@@ -1,24 +1,66 @@
 package com.example.demo.integration;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 
+import com.example.demo.component.SampleComponent;
+import com.example.demo.config.BatchConfig;
+import com.example.demo.tasklet.Tasklet1;
+import com.example.demo.tasklet.Tasklet2;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBatchTest
+@ContextConfiguration(classes = {BatchConfig.class, Tasklet1.class, Tasklet2.class, SampleComponent.class})
+@EnableAutoConfiguration
+@Slf4j
+@DisplayName("Tasklet1のIntegrationTest")
 class Tasklet1IntegrationTest {
+	
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
+		log.info("=== Tasklet1 IntegrationTest Start ===");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		
+		log.info("=== Tasklet1 IntegrationTest End ===");
 	}
 
 	@Test
-	void test() {
-		fail("まだ実装されていません");
+	@DisplayName("Tasklet1のステータスが完了すること")
+	void checkStatus() {
+		
+		// 準備
+		// Job起動パラメーター
+		JobParameters jobParams = new JobParametersBuilder()
+				.addString("param", "paramTest")
+				.toJobParameters();
+		
+		// テスト
+		JobExecution jobExecution = jobLauncherTestUtils.launchStep("Step1", jobParams);
+		
+		// 検証
+		// Stepのステータス
+		jobExecution.getStepExecutions().forEach(stepExecution -> assertThat(ExitStatus.COMPLETED).isEqualTo(stepExecution.getExitStatus()));
 	}
 
 }
